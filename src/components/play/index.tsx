dfd56e48faa42823bbca58b90b2828b9da7f629c
Blazephoenix <naik.tanmay283@gmail.com>
2024-07-06 14:44:37 +0530

    chore: Add start, signin, and play components

---
+++ src/components/play/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+export default function Index({}: Props) {
+  return (
+    <div className="container mx-auto bg-slate-950 p-10 min-h-screen">Test</div>
+  );
+}
---
+++ src/pages/start.tsx
@@ -0,0 +1,8 @@
+import Play from "@/components/play";
+import React from "react";
+
+type Props = {};
+
+export default function Start({}: Props) {
+  return <Play />;
+}
