e0f078d66f2021d4b04b7982cd4bb5b6ff7c3a06
Blazephoenix <naik.tanmay283@gmail.com>
2024-11-17 12:41:44 +0530

    clerk changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import "../styles/globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider>
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
